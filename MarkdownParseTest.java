802c2962027f4d8a34d30d9fc965d18cce966782
Maanasa64 <83880147+Maanasa64@users.noreply.github.com>
2022-04-22 09:30:43 -0700

    JUnit tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -26,4 +26,88 @@ public class MarkdownParseTest {
         actual = mark.getLinks(content);
         assertArrayEquals(toReturn.toArray(), actual.toArray());
     }
+    @Test
+    public void getLinks2() throws IOException{
+        ArrayList<String> toReturn = new ArrayList<>();
+        toReturn.add("https://something.com");
+        toReturn.add("some-page.html");
+        MarkdownParse mark = new MarkdownParse();
+        Path filePath = Path.of("test-file2.md");
+        String content = Files.readString(filePath);
+        ArrayList<String> actual = new ArrayList<>();
+        actual = mark.getLinks(content);
+        assertArrayEquals(toReturn.toArray(), actual.toArray());
+    }
+    @Test
+    public void getLinks3() throws IOException{
+        ArrayList<String> toReturn = new ArrayList<>();
+        // toReturn.add("https://something.com");
+        // toReturn.add("some-page.html");
+        MarkdownParse mark = new MarkdownParse();
+        Path filePath = Path.of("test-file3.md");
+        String content = Files.readString(filePath);
+        ArrayList<String> actual = new ArrayList<>();
+        actual = mark.getLinks(content);
+        assertArrayEquals(toReturn.toArray(), actual.toArray());
+    }
+    @Test
+    public void getLinks4() throws IOException{
+        ArrayList<String> toReturn = new ArrayList<>();
+        // toReturn.add("https://something.com");
+        // toReturn.add("some-page.html");
+        MarkdownParse mark = new MarkdownParse();
+        Path filePath = Path.of("test-file4.md");
+        String content = Files.readString(filePath);
+        ArrayList<String> actual = new ArrayList<>();
+        actual = mark.getLinks(content);
+        assertArrayEquals(toReturn.toArray(), actual.toArray());
+    }
+    @Test
+    public void getLinks5() throws IOException{
+        ArrayList<String> toReturn = new ArrayList<>();
+        // toReturn.add("https://something.com");
+        // toReturn.add("some-page.html");
+        MarkdownParse mark = new MarkdownParse();
+        Path filePath = Path.of("test-file5.md");
+        String content = Files.readString(filePath);
+        ArrayList<String> actual = new ArrayList<>();
+        actual = mark.getLinks(content);
+        assertArrayEquals(toReturn.toArray(), actual.toArray());
+    }
+    @Test
+    public void getLinks6() throws IOException{
+        ArrayList<String> toReturn = new ArrayList<>();
+        // toReturn.add("https://something.com");
+        // toReturn.add("some-page.html");
+        MarkdownParse mark = new MarkdownParse();
+        Path filePath = Path.of("test-file6.md");
+        String content = Files.readString(filePath);
+        ArrayList<String> actual = new ArrayList<>();
+        actual = mark.getLinks(content);
+        assertArrayEquals(toReturn.toArray(), actual.toArray());
+    }
+    @Test
+    public void getLinks7() throws IOException{
+        ArrayList<String> toReturn = new ArrayList<>();
+        // toReturn.add("https://something.com");
+        // toReturn.add("some-page.html");
+        MarkdownParse mark = new MarkdownParse();
+        Path filePath = Path.of("test-file7.md");
+        String content = Files.readString(filePath);
+        ArrayList<String> actual = new ArrayList<>();
+        actual = mark.getLinks(content);
+        assertArrayEquals(toReturn.toArray(), actual.toArray());
+    }
+    @Test
+    public void getLinks8() throws IOException{
+        ArrayList<String> toReturn = new ArrayList<>();
+        // toReturn.add("https://something.com");
+        // toReturn.add("some-page.html");
+        MarkdownParse mark = new MarkdownParse();
+        Path filePath = Path.of("test-file8.md");
+        String content = Files.readString(filePath);
+        ArrayList<String> actual = new ArrayList<>();
+        actual = mark.getLinks(content);
+        assertArrayEquals(toReturn.toArray(), actual.toArray());
+    }
 }
---
+++ test2-file.md
@@ -0,0 +1,4 @@
+# Title
+
+(https://docs.google.com/document/d/1LnSfvTG_Hn2fxDtFMuhBhJqsf9336Bm1ljux2Af9FqE/edit#heading=h.2ijxt4bhyfjb)[link1]
+[link2](https://something.com)
